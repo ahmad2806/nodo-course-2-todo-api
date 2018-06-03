fdcae164e84f49c3c77eb907344a0dc03c54383d
ahmad2806 <ahmadhashem2806@gmail.com>
2018-06-03 17:44:53 +0300

    Add Post /todo /user routs and refactor mongoose

---
+++ server/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+var app = express();
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    var todo = new Todo({
+        text: req.body.text
+    });
+    todo.save().then( (doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
+app.post('/users', (req, res) => {
+    var user = new User({
+        email: req.body.email
+    });
+    console.log(req.body);
+    console.log('next \n',user);
+    user.save().then( (doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+        
+    });
+});
+
+
+app.listen(3000, () => {
+    console.log('Started On port 3000');
+});
