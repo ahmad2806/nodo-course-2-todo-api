8a22d03a458278da91c051748d7a2ad38289049f
ahmad2806 <ahmadhashem2806@gmail.com>
2018-06-04 17:07:49 +0300

    files was modifed during a server erro

--- server/server.js
+++ server/server.js
@@ -1,70 +1,56 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const { ObjectID } = require('mongodb');
+var express = require('express');
+var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
-
-var { mongoose } = require('./db/mongoose');
-var { Todo } = require('./models/todo');
-var { User } = require('./models/user');
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
 var app = express();
-
-var port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
-    var todo = new Todo({
-        text: req.body.text
-    });
-    todo.save().then((doc) => {
-        res.send(doc);
-    }, (e) => {
-        res.status(400).send(e);
-    });
-});
+  var todo = new Todo({
+    text: req.body.text
+  });
 
-app.post('/users', (req, res) => {
-    var user = new User({
-        email: req.body.email
-    });
-    console.log(req.body);
-    console.log('next \n', user);
-    user.save().then((doc) => {
-        res.send(doc);
-    }, (e) => {
-        res.status(400).send(e);
-
-    });
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
 });
 
 app.get('/todos', (req, res) => {
-    Todo.find().then((todos) => {
-        res.send({ todos });
-    }, (e) => {
-        res.status(400).send(e);
-    });
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  });
 });
 
 app.get('/todos/:id', (req, res) => {
-    var id = req.params.id;
-    if (!ObjectID.isValid(id)) {
-        res.status(404).send();
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
     }
-    Todo.findById(id).then((todo) => {
-        if (!todo) {
-            res.status(404).send();
-        }
-        res.status(200).send({ todo });
-    }, (e) => res.status(400).send())
 
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
 });
-
 
 app.listen(port, () => {
-    console.log(`Starting on port ${port}`);
+  console.log(`Started up at port ${port}`);
 });
 
-module.exports = {
-    app
-}
+module.exports = {app};
