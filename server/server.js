5ef48653d46ff9f226a4f5d0b59646037ebb03ad
ahmad2806 <ahmadhashem2806@gmail.com>
2018-06-04 14:31:15 +0300

    Add GET todo route by id

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 
-var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
+var { mongoose } = require('./db/mongoose');
+var { Todo } = require('./models/todo');
+var { User } = require('./models/user');
 
 var app = express();
 app.use(bodyParser.json());
@@ -13,7 +14,7 @@ app.post('/todos', (req, res) => {
     var todo = new Todo({
         text: req.body.text
     });
-    todo.save().then( (doc) => {
+    todo.save().then((doc) => {
         res.send(doc);
     }, (e) => {
         res.status(400).send(e);
@@ -25,21 +26,35 @@ app.post('/users', (req, res) => {
         email: req.body.email
     });
     console.log(req.body);
-    console.log('next \n',user);
-    user.save().then( (doc) => {
+    console.log('next \n', user);
+    user.save().then((doc) => {
         res.send(doc);
     }, (e) => {
         res.status(400).send(e);
-        
+
     });
 });
 
 app.get('/todos', (req, res) => {
-    Todo.find().then( (todos) => {
-       res.send({todos}); 
+    Todo.find().then((todos) => {
+        res.send({ todos });
     }, (e) => {
         res.status(400).send(e);
     });
+});
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        res.status(404).send();
+    }
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            res.status(404).send();
+        }
+        res.status(200).send({ todo });
+    }, (e) => res.status(400).send())
+
 });
 
 
