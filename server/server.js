ef0f917c679c40313a64d8eaed79d97f74637798
ahmad2806 <ahmadhashem2806@gmail.com>
2018-06-05 18:19:28 +0300

    Patch route was added

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var { ObjectID } = require('mongodb');
+var _ = require('lodash');
 
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
@@ -51,6 +52,7 @@ app.get('/todos/:id', (req, res) => {
 
 app.delete('/todos/:id', (req, res) => {
   var id = req.params.id;
+
   if (!ObjectID.isValid(id)) {
     return res.status(404).send();
   }
@@ -59,7 +61,34 @@ app.delete('/todos/:id', (req, res) => {
     if (!todo) {
       return res.status(404).send();
     }
-    
+
+    res.send({ todo });
+  }, (e) => res.status(400).send());
+});
+
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;   //getting the id
+  var body = _.pick(req.body, ['text', 'completed']);//we define body as a subset of the things that the user will pass to us
+  //we dont want the user to be able to change all the fields in that object  
+
+  if (!ObjectID.isValid(id)) {  // checking if the id is valid
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {//update the completedAt property based on the completed property
+
+    body.completedAt = new Date().getTime();
+
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, { $set: body }, { new: true }).then((todo) => {//its like what we wrote in mongoDb-update file and we replaced returnOriginal = false with new =true
+    if (!todo) {
+      return res.status(404).send();
+    }
+
     res.send({todo});
   }, (e) => res.status(400).send());
 });
